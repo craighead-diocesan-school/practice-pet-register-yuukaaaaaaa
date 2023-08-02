012de5d210f7ef5cad93f5e115015e26c227915c
yuukaaaaaaa <sugiyayu93@craighead.school.nz>
2023-08-01 19:38:22 -0700

    Fixed the codes

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
 let petArray = [
-    {name: 'Luna', age: 5, speices: 'Dog'}
-    {name: 'Bella', age: 0, speices: 'Cat'}
-    {name: 'Charlie', age: 3, speices: 'Lizard'}
-    {name: 'Max', age: 12,  speices: 'Dog'}
-    {name: 'Rocky', age: 96, speices: 'Turtle'}
-    {name: 'Lucy', age: 4,  speices: 'Cat'}
-    {name: 'Bailey', age: 6,  speices: 'Fish'}
-    {name: 'Milo', age: 2,  speices: 'Hamster'}
-    {name: 'Cooper', age: 2,  speices: 'Duck'}
-    {name: 'Coco', age: 10, speices: 'Dog'}
+    {name: 'Luna', age: 5, speices: 'Dog'},
+    {name: 'Bella', age: 0, speices: 'Cat'},
+    {name: 'Charlie', age: 3, speices: 'Lizard'},
+    {name: 'Max', age: 12,  speices: 'Dog'},
+    {name: 'Rocky', age: 96, speices: 'Turtle'},
+    {name: 'Lucy', age: 4,  speices: 'Cat'},
+    {name: 'Bailey', age: 6,  speices: 'Fish'},
+    {name: 'Milo', age: 2,  speices: 'Hamster'},
+    {name: 'Cooper', age: 2,  speices: 'Duck'},
+    {name: 'Coco', age: 10, speices: 'Dog'},
 ]
 
 function addPets () {
+  // Ask for the name, species and age, and add to petArray
     let name = prompt(" What's your pet's name? ")
-    let species = prompt("What's kind of your dog?")
-    let age = prompt('How old is your dog?')
+    let species = prompt("What's kind of your pet?")
+    let age = prompt('How old is your pet?')
     let pet = {
         name: name,
         age: age,
         species: species,
     }
-    PermissionStatus.push(pet)
+    petArray.push(pet)
 }
 
-function showPets (){
-    alert (petArray)
-}
+function showPets(){
+    // use a for..of loop to go through the pet array
+    for (let currentPet of petArray){
+    alert(currentPet.name)
+    }
+    // output the name of the pet
+  }
+  
 
 function removePets() { 
     //Keep asking which subjects they want to delete until told to stop.
@@ -36,10 +42,11 @@ function removePets() {
 }
 
 function searchPets() {
+    // Ask for the name to search and show petname + was found if it can find it otherwise show petname + was not found.
     let searchName = prompt('What is the name of pet do you want to search?')
     let searchResult = false
-    for (let name of petArray) {
-      if (name == searchName) {
+    for (let pet of petArray) {
+      if (pet.name == searchName) {
         searchResult = true 
       }
     }
@@ -50,21 +57,3 @@ function searchPets() {
       alert(searchName + ' was not found...')
     }
 }
-
-function searchSpecies() {
-    let searchSpeices = prompt('What is the species of pet do you want to search?')
-    let searchResult = false
-    for (let name of petArray) {
-      if (name == searchSpecies) {
-        searchResult = true 
-      }
-    }
-  
-    if (searchResult == true){
-      alert(searchSpecies + ' was found! ')
-    } else{
-      alert(searchSpecies + ' was not found...')
-    }
-}
-
-
