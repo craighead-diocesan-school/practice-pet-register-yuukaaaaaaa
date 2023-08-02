b0b03bee663aecb33e5386ea61c94cb6db57398a
yuukaaaaaaa <sugiyayu93@craighead.school.nz>
2023-08-01 19:51:17 -0700

    Fixed codes

--- script.js
+++ script.js
@@ -3,12 +3,6 @@ let petArray = [
     {name: 'Bella', age: 0, speices: 'Cat'},
     {name: 'Charlie', age: 3, speices: 'Lizard'},
     {name: 'Max', age: 12,  speices: 'Dog'},
-    {name: 'Rocky', age: 96, speices: 'Turtle'},
-    {name: 'Lucy', age: 4,  speices: 'Cat'},
-    {name: 'Bailey', age: 6,  speices: 'Fish'},
-    {name: 'Milo', age: 2,  speices: 'Hamster'},
-    {name: 'Cooper', age: 2,  speices: 'Duck'},
-    {name: 'Coco', age: 10, speices: 'Dog'},
 ]
 
 function addPets () {
@@ -37,7 +31,7 @@ function removePets() {
     //Keep asking which subjects they want to delete until told to stop.
     let deletePet = prompt('Which pet do you want to delete?')
     while(deletePet!= 'stop'){
-      pet.splice(index, 1)
+      pet.splice(indextoRemove, 1)
     }
 }
 
